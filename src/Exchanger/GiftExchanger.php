614c5a74d1188a23b72a0085f5c4cbf903a66523
Brian Feaver <brian.feaver@gmail.com>
2020-11-02 09:24:36 -0800

    Add first logic

--- src/Exchanger/GiftExchanger.php
+++ src/Exchanger/GiftExchanger.php
@@ -4,8 +4,16 @@ namespace App\Exchanger;
 
 class GiftExchanger
 {
-    public function getAssignment(string $name, array $excludeList, array $fullList)
+    public function getAssignment(string $name, array $excludeList, array $fullList): string
     {
+        $excludeList += [$name];
 
+        $fullList = array_filter($fullList, function ($v) use ($excludeList) {
+            return !in_array($v, $excludeList);
+        });
+
+        $key = array_rand($fullList);
+
+        return $fullList[$key];
     }
 }
--- tests/Exchanger/GiftExchangerTest.php
+++ tests/Exchanger/GiftExchangerTest.php
@@ -7,8 +7,12 @@ use PHPUnit\Framework\TestCase;
 
 class GiftExchangerTest extends TestCase
 {
-
     public function testGetAssignment()
     {
+        $exchanger = new GiftExchanger();
+
+        $assignment = $exchanger->getAssignment('foo', [], ['foo', 'bar', 'foobar', 'barfoo']);
+
+        self::assertNotEquals('foo', $assignment);
     }
 }
