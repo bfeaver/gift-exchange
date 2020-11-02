4de90cd3faf2acf908dc9b0c3f45606ce2ba0e12
Brian Feaver <brian.feaver@gmail.com>
2020-11-02 10:21:51 -0800

    Throw exception on empty list to randomizer

--- src/Exchanger/Randomizer/ArrayRandRandomizer.php
+++ src/Exchanger/Randomizer/ArrayRandRandomizer.php
@@ -6,6 +6,10 @@ class ArrayRandRandomizer implements ExchangeRandomizer
 {
     public function getValue(array $list): string
     {
+        if (empty($list)) {
+            throw new \InvalidArgumentException('List cannot be empty.');
+        }
+
         $key = array_rand($list);
 
         return $list[$key];
